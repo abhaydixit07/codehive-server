654f39263edfd01a68f0103aac68b0f3cdaf4a4f
Khagesh Sharma <khageshsharma24@gmail.com>
2024-11-03 00:28:00 +0530

    Combined the backend for code-editor and video calling

--- server.js
+++ server.js
@@ -2,74 +2,131 @@ const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
 const cors = require("cors");
-
+const { v4: uuidv4 } = require("uuid");
 const app = express();
 const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000", 
+    origin: "http://localhost:3000",
     methods: ["GET", "POST"],
   },
 });
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-
-const rooms = {}; 
+const rooms = {};
 
 io.on("connection", (socket) => {
   console.log("A user connected:", socket.id);
 
-  socket.on("join_room", (roomId) => {
-    socket.join(roomId);
-    console.log(`User ${socket.id} joined room ${roomId}`);
+  socket.on("create_room", (callback) => {
+    const roomId = uuidv4();
+    rooms[roomId] = {
+      users: {},
+      code: "",
+      hostId: socket.id,
+    };
+    callback(roomId);
+  });
 
+  socket.on("join_room", ({ roomId, userName }) => {
     if (!rooms[roomId]) {
-      rooms[roomId] = {};
+      socket.emit("room_not_found");
+      return;
     }
 
-    rooms[roomId][socket.id] = { code: "", cursorPosition: null };
+    socket.join(roomId);
 
-    // Send current room state to the new user
-    socket.emit("initial_data", { code: rooms[roomId][socket.id].code, users: Object.keys(rooms[roomId]) });
-
-    
-    io.in(roomId).emit("update_users", Object.keys(rooms[roomId]));
+    rooms[roomId].users[socket.id] = {
+      userName,
+      videoEnabled: true,
+      audioEnabled: true,
+      code: rooms[roomId].code || "",
+      cursorPosition: null,
+    };
+    socket.emit("initial_room_data", {
+      code: rooms[roomId].code,
+      users: rooms[roomId].users,
+    });
+    socket.to(roomId).emit("user_joined", {
+      userId: socket.id,
+      userName,
+      users: rooms[roomId].users,
+    });
   });
 
   socket.on("code_change", ({ roomId, code, cursorPosition }) => {
-    if (rooms[roomId] && rooms[roomId][socket.id]) {
-   
-      rooms[roomId][socket.id].code = code;
-      rooms[roomId][socket.id].cursorPosition = cursorPosition;
+    if (rooms[roomId]?.users[socket.id]) {
+      rooms[roomId].code = code;
+      rooms[roomId].users[socket.id].cursorPosition = cursorPosition;
 
-
-      socket.to(roomId).emit("receive_code_change", { code, cursorPosition });
+      socket.to(roomId).emit("receive_code_change", {
+        code,
+        cursorPosition,
+        userId: socket.id,
+      });
     }
   });
 
   socket.on("cursor_position_change", ({ roomId, cursorPosition }) => {
-    if (rooms[roomId] && rooms[roomId][socket.id]) {
-      
-      rooms[roomId][socket.id].cursorPosition = cursorPosition;
+    if (rooms[roomId]?.users[socket.id]) {
+      rooms[roomId].users[socket.id].cursorPosition = cursorPosition;
 
-      socket.to(roomId).emit("receive_cursor_position", { userId: socket.id, cursorPosition });
+      socket.to(roomId).emit("receive_cursor_position", {
+        userId: socket.id,
+        cursorPosition,
+      });
+    }
+  });
+  socket.on("sending_signal", ({ userToSignal, signal, roomId }) => {
+    io.to(userToSignal).emit("user_joined_with_signal", {
+      signal,
+      callerID: socket.id,
+      userName: rooms[roomId]?.users[socket.id]?.userName,
+    });
+  });
+
+  socket.on("returning_signal", ({ signal, callerID }) => {
+    io.to(callerID).emit("receiving_returned_signal", {
+      signal,
+      id: socket.id,
+    });
+  });
+  socket.on("toggle_video", ({ roomId, enabled }) => {
+    if (rooms[roomId]?.users[socket.id]) {
+      rooms[roomId].users[socket.id].videoEnabled = enabled;
+      socket.to(roomId).emit("user_toggled_video", {
+        userId: socket.id,
+        enabled,
+      });
     }
   });
 
+  socket.on("toggle_audio", ({ roomId, enabled }) => {
+    if (rooms[roomId]?.users[socket.id]) {
+      rooms[roomId].users[socket.id].audioEnabled = enabled;
+      socket.to(roomId).emit("user_toggled_audio", {
+        userId: socket.id,
+        enabled,
+      });
+    }
+  });
   socket.on("disconnect", () => {
     console.log("User disconnected:", socket.id);
 
     for (const roomId in rooms) {
-      if (rooms[roomId][socket.id]) {
-        delete rooms[roomId][socket.id];
-        io.in(roomId).emit("update_users", Object.keys(rooms[roomId]));
+      if (rooms[roomId].users[socket.id]) {
+        delete rooms[roomId].users[socket.id];
 
-        if (Object.keys(rooms[roomId]).length === 0) {
+        if (Object.keys(rooms[roomId].users).length === 0) {
           delete rooms[roomId];
+        } else {
+          socket.to(roomId).emit("user_left", {
+            userId: socket.id,
+            users: rooms[roomId].users,
+          });
         }
         break;
       }
@@ -77,13 +134,12 @@ io.on("connection", (socket) => {
   });
 });
 
-
+// Basic health check endpoint
 app.get("/", (req, res) => {
-  res.send("Server is up and running!");
+  res.send("Server is running!");
 });
 
-// Start the server
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
-  console.log(`Server listening on http://localhost:${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
